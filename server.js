d45b164c08f7350e9c38b08e028417d51fb55e75
Lulu Dong <luluberry8@gmail.com>
2024-09-26 16:20:14 -0700

    modify server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require("http");
 const url = require("url");
-const fs = require("fs");
-const path = require("path");
 const { getDate } = require("./modules/utils");
 const { greeting } = require("./lang/en/en");
 
 const port = process.env.PORT || 3000;
+
+let fileContent = "";
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
@@ -25,34 +25,21 @@ const server = http.createServer((req, res) => {
   } else if (pathname === "/COMP4537/writeFile/") {
     const { text } = parsedUrl.query;
     if (text) {
-      fs.appendFile("file.txt", text + "\n", (err) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
-          res.end("Error writing to file");
-        } else {
-          res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-          res.end("Text appended to file successfully");
-        }
-      });
+      fileContent += text + "\n";
+      res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Text appended to file successfully");
     } else {
       res.writeHead(400, { "Content-Type": "text/plain; charset=utf-8" });
       res.end("Please provide text in the query string");
     }
   } else if (pathname === "/COMP4537/readFile/file.txt") {
-    fs.readFile("file.txt", "utf8", (err, data) => {
-      if (err) {
-        if (err.code === "ENOENT") {
-          res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
-          res.end("404 File Not Found");
-        } else {
-          res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
-          res.end("Error reading file");
-        }
-      } else {
-        res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-        res.end(data);
-      }
-    });
+    if (fileContent) {
+      res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end(fileContent);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("404 File Not Found: file.txt");
+    }
   } else {
     res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
     res.end("404 Not Found");
